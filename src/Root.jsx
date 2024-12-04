9e52a5bf7ad8106bdf4046aac8d27cf6d758b5fb
Abrar <ahmedabrararian@gmail.com>
2024-12-04 17:14:47 +0600

    Set up Vite Project with React-Router, TailwindCSS & DaisyUI

---
+++ src/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Root = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Root;
