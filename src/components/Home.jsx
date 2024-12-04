40f8020daf1996f890f4802ef4e880845d80a97a
Abrar <ahmedabrararian@gmail.com>
2024-12-05 01:43:07 +0600

    Added react-slick, slick-carousel, roboto condensed font, made slider banner for Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
+import Banner from "./HomeComponents/Banner";
+
 const Home = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="bg-primary">
+        <div className="max-w-7xl mx-auto px-5 py-10">
+          <Banner></Banner>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
