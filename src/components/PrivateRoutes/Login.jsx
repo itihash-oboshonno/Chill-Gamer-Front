93534a08319f44bcd828af6572f4dbaa8b505e22
Abrar <ahmedabrararian@gmail.com>
2024-12-05 07:59:26 +0600

    Added Login Functionality

--- src/components/PrivateRoutes/Login.jsx
+++ src/components/PrivateRoutes/Login.jsx
@@ -1,12 +1,53 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
+
+  const {userLogin, loginViaGoogle, setLoading} = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [errorMess, setErrorMess] = useState("");
+
+  const from = location.state?.from?.pathname || "/";
+
+  const handleLoginGoogle = (e) => {
+    e.preventDefault();
+    loginViaGoogle()
+    .then((result) => {
+      // toast.success(`${result.user.displayName} logged in successfully!`);
+      navigate(from, { replace: true });
+    })
+    .catch((error) => {
+      // toast.error(error.message);
+    });
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    userLogin(email, password)
+      .then((result) => {
+        // toast.success(`${result.user.displayName} logged in successfully!`);
+        setLoading(true);
+        navigate(from, { replace: true });
+        setLoading(false);
+      })
+      .catch((error) => {
+        setErrorMess(error.message);
+        // toast.error(error.message);
+      });
+  };
+
   return (
     <div className="">
       <div className="max-w-6xl mx-auto px-5">
         <h2 className="font-bold text-4xl text-center my-10">Login</h2>
         <div className="border-2 rounded-2xl py-5 grid justify-center">
-          <form className="grid justify-center">
+          <form className="grid justify-center" onSubmit={handleLogin}>
             <div>
               <p className="my-2">Email</p>
               <input
@@ -34,7 +75,7 @@ const Login = () => {
               />
           </form>
           <p className="text-center">or</p>
-          <button className="rounded-lg p-2 border-2 my-5 mx-auto px-5">
+          <button onClick={handleLoginGoogle} className="rounded-lg p-2 border-2 my-5 mx-auto px-5">
             Sign In With Google
           </button>
           <p className="text-center">Don't have an account? Please <Link to="/signup" className="underline">Register Here</Link></p>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase.init";
+import { GoogleAuthProvider } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
@@ -9,21 +10,50 @@ const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   const createUserWithPass = (email, password) => {
-    setLoading(true);
+    // setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   }
+
+  const userLogin = (email, password) => {
+    // setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleProvider = new GoogleAuthProvider();
+  const loginViaGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const updateUserData = (updatedData) => {
     return updateProfile(auth.currentUser, updatedData);
   }
+
+  const userLogout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
   const authInfo = {
     currentUser,
     loading,
     setLoading,
     createUserWithPass,
+    userLogin,
+    loginViaGoogle,
     updateUserData,
     setCurrentUser,
+    userLogout,
   };
 
   return (
