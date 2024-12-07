c55be81f4405acc8f1be21c05c9547de17168a7f
Abrar <ahmedabrararian@gmail.com>
2024-12-07 08:13:23 +0600

    Created basic Watchlist functionality

--- src/components/PrivateRoutes/GameWatchlist.jsx
+++ src/components/PrivateRoutes/GameWatchlist.jsx
@@ -1,7 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+
 const GameWatchlist = () => {
+    const {currentUser} = useContext(AuthContext);
+    const wishMail = currentUser.email;
+    const [fetchedData, setFetchedData] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/wishlist?searchParams=${wishMail}`)
+        .then(res => res.json())
+        .then(data => {
+            setFetchedData(data)
+        })
+
+    }, [wishMail])
+
     return (
         <div>
-            
+            {
+                fetchedData ? <div><p>{fetchedData.length}</p></div> : <div><p>You haven't added any games to your watchlist yet.</p></div>
+            }
         </div>
     );
 };
